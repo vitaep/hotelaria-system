d67dbe032cc8f917161cf57182f52480b132e2fe
Vitor Souza <souzavitorb1@gmail.com>
2025-03-24 15:35:50 -0300

    first commit

---
+++ src/main/java/dev/vitaep/hotelariaSystem/HotelariaSystemApplication.java
@@ -0,0 +1,13 @@
+package dev.vitaep.hotelariaSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelariaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelariaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/vitaep/hotelariaSystem/HotelariaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vitaep.hotelariaSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelariaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
