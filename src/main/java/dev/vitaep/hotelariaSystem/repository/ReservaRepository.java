86ae5fd6b7ad598e34c0500bc45c00bdac7be771
Vitor Souza <souzavitorb1@gmail.com>
2025-03-26 07:02:51 -0300

    feat: added the ReservaRepository

---
+++ src/main/java/dev/vitaep/hotelariaSystem/repository/ReservaRepository.java
@@ -0,0 +1,10 @@
+package dev.vitaep.hotelariaSystem.repository;
+
+import dev.vitaep.hotelariaSystem.entity.Reserva;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+
+
+
+}
