86ca142bc866053071a1f5eb538ad928a6f87b84
Vitor Souza <souzavitorb1@gmail.com>
2025-03-26 07:03:18 -0300

    feat: added the RoomRepository

---
+++ src/main/java/dev/vitaep/hotelariaSystem/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package dev.vitaep.hotelariaSystem.repository;
+
+import dev.vitaep.hotelariaSystem.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+
+
+}
